8a2c5f9e43fabcdea5f741cd44c76900c3b89def
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-12-06 19:01:17 -0800

    reverted

--- migrations/20230913075958-create-videos-table.js
+++ migrations/20230913075958-create-videos-table.js
@@ -2,79 +2,37 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-    });
-
-  
     await queryInterface.createTable('videos', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER
       },
       filename: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
       },
       title: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
       },
       category: {
-        type: Sequelize.STRING,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users',
-          key: 'id',
-        },
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: true,
-        type: Sequelize.DATE,
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: true,
-        type: Sequelize.DATE,
-      },
+        type: Sequelize.DATE
+      }
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-  
     await queryInterface.dropTable('videos');
-
-  
-    await queryInterface.dropTable('users');
-  },
+  }
 };
